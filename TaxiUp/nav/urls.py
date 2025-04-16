994a9b05d039037b0aae2caea3e3c5abd23226b5
mpho-n <mphom2006@gmail.com>
2025-04-16 02:59:16 +0200

    nav app + html added

--- TaxiUp/nav/urls.py
+++ TaxiUp/nav/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.home, name='home'),
+    path('', views.main, name='home'),
+    path('main', views.main, name='home'),
+    path('page', views.page, name='home'),
+    path('place', views.place, name='home'),
 ]
--- TaxiUp/nav/views.py
+++ TaxiUp/nav/views.py
@@ -8,3 +8,13 @@ import math
 
 def displacement(p1,p2):
     return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+
+
+def main(request):
+    return render(request, 'nav/main.html')
+
+def page(request):
+    return render(request, 'nav/page.html')
+
+def place(request):
+    return render(request, 'nav/place.html')
