969a78ca8abb03927533303395782aacb47aa1ad
111nation <tafara.k@outlook.com>
2025-04-20 22:35:13 +0000

    Finished with request layout

--- TaxiUp/nav/static/nav/js/nav.js
+++ TaxiUp/nav/static/nav/js/nav.js
@@ -14,16 +14,27 @@ document.addEventListener("DOMContentLoaded", function () {
 		["place", document.getElementById("place-page")],
 		["event", document.getElementById("event-page")],
 		["search", document.getElementById("search-page")],
+		["trip", document.getElementById("trip-page")],
 	]);
 
 	const buttons = new Map([
 		["request", document.getElementById("request-ride")],
 		["go-to", document.getElementById("go-to")],
+		["trip", document.getElementById("cancel-trip")],
 	]);
 
 	let search = document.getElementById("search");
 	let backButton = document.getElementById("back");
 	let prevPage = "";
+
+	function toggleSearch(show) {
+		// Handles showing and hiding of search bar
+		if (show === true) {
+			search.style.display = "initial";
+		} else {
+			search.style.display = "none";
+		}
+	}
 
 	function toggleButton(button) {
 		// Makes showing and hiding the Request, and Go To button more manageable
@@ -77,14 +88,23 @@ document.addEventListener("DOMContentLoaded", function () {
 			case "event":
 				toggleButton("go-to");
 				break;
+			case "trip":
+				toggleButton("trip");
+				break;
 		}
 
 		// Show back button only non main pages
-		if (page !== "main") {
+		if (page !== "main" && page !== "trip") {
 			backButton.style.display = "initial";
 		} else {
-			toggleButton("request");
 			backButton.style.display = "none";
+		}
+
+		// Dont show search in trip page
+		if (page !== "trip") {
+			toggleSearch(true);
+		} else {
+			toggleSearch(false);
 		}
 
 		togglePage.lastPage = page;
@@ -111,7 +131,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 			document.getElementById('headPlace').textContent = name;
 			document.getElementById('placeContainer1').style.backgroundImage = `url('static/nav/images/${placeID}.jpg')`;
-			document.getElementById('description').innerHTML = `distance: ${disp}km<br>cost: R${cost}`;
+			document.getElementById('description').innerHTML = `Distance: ${disp}km<br>Trip fare: R${cost}`;
 			document.getElementById('destination').value = name;
 			document.getElementById('destination').innerHTML = placeID;
 
@@ -170,5 +190,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	search.addEventListener("search", requestSearch);
 
-	togglePage("main");
+	togglePage("trip");
 });
